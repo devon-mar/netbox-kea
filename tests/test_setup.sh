b37c284380c08ce562ca60ab9843816853cbf0f3
Devon Mar <devonm@mdmm.ca>
2024-04-03 10:42:14 -0700

    Use docker compose v2

--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -12,7 +12,7 @@ echo "Copying whl"
 WHL_FILE=$(ls ./dist/ | grep .whl)
 cp  "./dist/$WHL_FILE" ./tests/docker/
 
-echo "Running docker-compose up"
+echo "Running docker compose up"
 cd ./tests/docker/
-docker-compose build --build-arg "FROM=netboxcommunity/netbox:$NETBOX_CONTAINER_TAG" --build-arg "WHL_FILE=$WHL_FILE"
-docker-compose up -d
+docker compose build --build-arg "FROM=netboxcommunity/netbox:$NETBOX_CONTAINER_TAG" --build-arg "WHL_FILE=$WHL_FILE"
+docker compose up -d
