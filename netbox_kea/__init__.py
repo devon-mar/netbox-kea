180b74dc71cb793280d01b359d6678cd46089fc6
Devon Mar <devonm@mdmm.ca>
2024-09-10 08:10:27 +0000

    Bump version to 1.0.1

--- netbox_kea/__init__.py
+++ netbox_kea/__init__.py
@@ -5,7 +5,7 @@ class NetBoxKeaConfig(PluginConfig):
     name = "netbox_kea"
     verbose_name = "Kea"
     description = "Kea integration for NetBox"
-    version = "1.0.0"
+    version = "1.0.1"
     base_url = "kea"
     default_settings = {"kea_timeout": 30}
 
