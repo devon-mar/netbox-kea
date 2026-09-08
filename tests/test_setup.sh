c9d83c75522d357660bdd24b69ccd4857ede5dad
Devon Mar <devonm@mdmm.ca>
2026-09-07 23:17:46 -0700

    Fix SSL tests for Python 3.13
    SSL tests fail with Python 3.13 (NetBox v4.6).
    ```
    FAILED tests/test_netbox_kea_api_server.py::test_server_create_basic_auth - pynetbox.core.query.RequestError: The request failed with code 400 Bad Request: {'dhcp6_url': ['Unable to get DHCPv6 version: SSLError(MaxRetryError("HTTPSConnectionPool(host=\'kea-dhcp6\', port=8003): Max retries exceeded with url: / (Caused by SSLError(SSLCertVerificationError(1, \'[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: Basic Constraints of CA cert not marked critical (_ssl.c:1081)\')))"))']}
    ```
    ```
    FAILED tests/test_netbox_kea_api_server.py::test_server_create_basic_auth - pynetbox.core.query.RequestError: The request failed with code 400 Bad Request: {'dhcp6_url': ['Unable to get DHCPv6 version: SSLError(MaxRetryError("HTTPSConnectionPool(host=\'kea-dhcp6\', port=8003): Max retries exceeded with url: / (Caused by SSLError(SSLCertVerificationError(1, \'[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: CA cert does not include key usage extension (_ssl.c:1081)\')))"))']}
    ```
    
    Mark basic constraints as critical and add `keyUsage`.

--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 echo "Generating certs"
 mkdir ./tests/docker/certs/
-openssl req -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -keyout ./tests/docker/certs/ca.key -out ./tests/docker/certs/ca.crt -addext "basicConstraints=CA:true" -subj "/CN=Test CA"
+openssl req -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -keyout ./tests/docker/certs/ca.key -out ./tests/docker/certs/ca.crt -addext "basicConstraints=critical,CA:true" -addext "keyUsage=critical,keyCertSign" -subj "/CN=Test CA"
 openssl req -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -CA ./tests/docker/certs/ca.crt -CAkey ./tests/docker/certs/ca.key -addext "basicConstraints=CA:false" -keyout ./tests/docker/certs/netbox.key -out ./tests/docker/certs/netbox.crt -addext "subjectAltName=DNS:netbox" -subj "/CN=netbox"
 openssl req -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -CA ./tests/docker/certs/ca.crt -CAkey ./tests/docker/certs/ca.key -addext "basicConstraints=CA:false" -keyout ./tests/docker/certs/dhcp6.key -out ./tests/docker/certs/dhcp6.crt -addext "subjectAltName=DNS:kea-dhcp6" -subj "/CN=kea-dhcp6"
 openssl req -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -CA ./tests/docker/certs/ca.crt -CAkey ./tests/docker/certs/ca.key -addext "basicConstraints=CA:false" -keyout ./tests/docker/certs/dhcp4.key -out ./tests/docker/certs/dhcp4.crt -addext "subjectAltName=DNS:kea-dhcp4" -subj "/CN=kea-dhcp4"
